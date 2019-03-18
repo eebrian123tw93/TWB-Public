404a0b532ced4fc1b921e5dce8db4bf84e645bf7
CONWAY <kable2580@gmail.com>
2019-03-18 14:10:03 +0800

    post article

---
+++ Shuo/src/main/java/com/cb/Shuo/model/ArticleModel.java
@@ -0,0 +1,41 @@
+package com.cb.Shuo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "articles")
+@Data
+public class ArticleModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "v_key")
+  private Long vKey;
+
+  @Column(name = "article_id", columnDefinition = "varchar(30)")
+  private String articleId;
+
+  @Column(name = "content", columnDefinition = "varchar(5000)")
+  private String content;
+
+  @Column(name = "images", columnDefinition = "varchar(1000)")
+  private String images;
+
+  @Column(name = "user_id", columnDefinition = "varchar(255)")
+  private String userId;
+
+  @Column(name = "title", columnDefinition = "varchar(50)")
+  private String title;
+
+  @Column(name = "points", columnDefinition = "int(11)")
+  private int points = 0;
+
+  @Column(name = "views", columnDefinition = "int(11)")
+  private int views = 0;
+
+  @Column(name = "create_time", columnDefinition = "datetime")
+  private LocalDateTime createTime;
+}
---
+++ Shuo/src/main/java/com/cb/Shuo/service/ArticlePostService.java
@@ -0,0 +1,26 @@
+package com.cb.Shuo.service;
+
+import com.cb.Shuo.dao.ArticleDao;
+import com.cb.Shuo.model.ArticleModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticlePostService {
+  private static final Logger logger = LoggerFactory.getLogger(ArticlePostService.class);
+
+  private final ArticleDao articleDao;
+
+  @Autowired
+  public ArticlePostService(ArticleDao articleDao) {
+    this.articleDao = articleDao;
+  }
+
+  public void postArticle(ArticleModel articleModel) {
+    logger.info("postArticle");
+    // todo: fix date time problem
+    articleDao.save(articleModel);
+  }
+}
