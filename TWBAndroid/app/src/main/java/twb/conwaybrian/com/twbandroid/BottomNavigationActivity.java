b55d857e6a13610945e7353ef8cab67f9e5643e9
brian <eebrian123tw93@gmail.com>
2019-03-19 11:59:56 +0800

    bottom navigation view

---
+++ TWBAndroid/app/src/main/java/twb/conwaybrian/com/twbandroid/BottomNavigationActivity.java
@@ -0,0 +1,25 @@
+package twb.conwaybrian.com.twbandroid;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class BottomNavigationActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        });
+    }
+}
