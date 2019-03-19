ac99383609f23e9845bbf0fe2f2581d03848d64e
brianlu <eebrian123tw93@gmail.com>
2019-03-19 13:16:36 +0800

    navigation view

--- TWBAndroid/app/src/main/java/twb/conwaybrian/com/twbandroid/BottomNavigationActivity.java
+++ TWBAndroid/app/src/main/java/twb/conwaybrian/com/twbandroid/BottomNavigationActivity.java
@@ -2,11 +2,12 @@ package twb.conwaybrian.com.twbandroid;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class BottomNavigationActivity extends AppCompatActivity {
+public class BottomNavigationActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private BottomNavigationView bottomNavigationView;
 
@@ -15,11 +16,28 @@ public class BottomNavigationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
         bottomNavigationView=findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                return false;
-            }
-        });
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new HomeFragment()).commit();
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment;
+        switch (menuItem.getItemId()){
+            case R.id.profile:
+                fragment=new ProfileFragment();
+                break;
+            case R.id.home:
+                fragment=new HomeFragment();
+                break;
+            default:
+                fragment=new HomeFragment();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,fragment).commit();
+        return true;
+    }
+
 }
