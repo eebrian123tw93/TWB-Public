0dc8d1beef5a45f54a85f85aee87f39cb6566c4a
brian <eebrian123tw93@gmail.com>
2019-03-22 16:59:53 +0800

    vv

--- TWBAndroid/app/src/main/java/twb/conwaybrian/com/twbandroid/presenter/ArticlePresenter.java
+++ TWBAndroid/app/src/main/java/twb/conwaybrian/com/twbandroid/presenter/ArticlePresenter.java
@@ -2,7 +2,7 @@ package twb.conwaybrian.com.twbandroid.presenter;
 
 import twb.conwaybrian.com.twbandroid.view.ArticleView;
 
-public class ArticlePresenter {
+public class ArticlePresenter extends TWBPresenter {
     private ArticleView articleView;
     public ArticlePresenter(ArticleView articleView){
         this.articleView=articleView;
