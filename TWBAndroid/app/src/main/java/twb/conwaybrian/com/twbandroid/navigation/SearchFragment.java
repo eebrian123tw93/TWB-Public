6061ad4a1d4513d8b5e372bfed25d031f02a157b
brian <eebrian123tw93@gmail.com>
2019-04-01 23:19:34 +0800

    search MVP

--- TWBAndroid/app/src/main/java/twb/conwaybrian/com/twbandroid/navigation/SearchFragment.java
+++ TWBAndroid/app/src/main/java/twb/conwaybrian/com/twbandroid/navigation/SearchFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +18,36 @@ import twb.conwaybrian.com.twbandroid.view.SearchView;
 
 public class SearchFragment extends Fragment implements SearchView {
     private SearchPresenter searchPresenter;
-
+    android.widget.SearchView searchView;
+    RecyclerView searchRecyclerView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        searchView=view.findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new android.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchPresenter.clear();
+                searchPresenter.search(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchPresenter.clear();
+                searchPresenter.search(newText);
+                return false;
+            }
+        });
+
+        searchRecyclerView = view.findViewById(R.id.search_recyclerview_view);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        searchRecyclerView.setLayoutManager(layoutManager);
         searchPresenter = new SearchPresenter(this);
+
         return view;
     }
 
@@ -30,4 +56,10 @@ public class SearchFragment extends Fragment implements SearchView {
 
         FancyToast.makeText(getContext(), message, FancyToast.LENGTH_SHORT, type, false).show();
     }
+
+    @Override
+    public void onSetArticleListRecyclerAdapter(RecyclerView.Adapter adapter) {
+        searchRecyclerView.setAdapter(adapter);
+    }
+
 }
--- TWBAndroid/app/src/main/java/twb/conwaybrian/com/twbandroid/presenter/SearchPresenter.java
+++ TWBAndroid/app/src/main/java/twb/conwaybrian/com/twbandroid/presenter/SearchPresenter.java
@@ -1,11 +1,67 @@
 package twb.conwaybrian.com.twbandroid.presenter;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import retrofit2.Response;
+import twb.conwaybrian.com.twbandroid.adatper.ArticleListRecycleViewAdapter;
+import twb.conwaybrian.com.twbandroid.model.Article;
+import twb.conwaybrian.com.twbandroid.shuoApi.ShuoApiService;
 import twb.conwaybrian.com.twbandroid.view.SearchView;
 
 public class SearchPresenter extends TWBPresenter {
     private SearchView searchView;
-
+    private ArticleListRecycleViewAdapter articleListRecycleViewAdapter;
     public SearchPresenter(SearchView searchView) {
         this.searchView = searchView;
+        articleListRecycleViewAdapter = new ArticleListRecycleViewAdapter(context);
+        searchView.onSetArticleListRecyclerAdapter(articleListRecycleViewAdapter);
+    }
+    public void search(String query){
+        Observer<Response<JsonArray>> observer = new Observer<Response<JsonArray>>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Response<JsonArray> response) {
+                if (response.isSuccessful()) {
+                    JsonArray jsonArray = response.body();
+                    System.out.println(jsonArray);
+                    Type listType = new TypeToken<List<Article>>() {
+                    }.getType();
+                    List<Article> articleList = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create().fromJson(jsonArray, listType);
+                    articleListRecycleViewAdapter.addArticles(articleList);
+
+                } else {
+                    searchView.onSetMessage("loading failed", FancyToast.ERROR);
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                searchView.onSetMessage(e.getMessage(), FancyToast.ERROR);
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+        ShuoApiService.getInstance().getUserPostHistory(observer,user,false);
+
+    }
+
+    public void clear(){
+        articleListRecycleViewAdapter.clear();
     }
 }
