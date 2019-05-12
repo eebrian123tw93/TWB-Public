acdd0968280d7a6cee4cad13953003595c864ad5
brian <eebrian123tw93@gmail.com>
2019-05-12 22:32:31 +0800

    forgot password

---
+++ TWB-ios/TWB-ios/TWBBase/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  TWB-ios
+//
+//  Created by Brian on 2019/5/12.
+//  Copyright © 2019 Brian. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
